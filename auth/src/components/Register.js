5f25937fc101a9a92043a2c64b7a22772ad70c81
sumonmitrasm <sumonmitrasm@gmail.com>
2024-02-29 17:24:09 +0600

    Updaet register

--- auth/src/components/Register.js
+++ auth/src/components/Register.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 export default class Register extends Component {
+  state={
+    name:"",
+    email:"",
+    password:"",
+    password_confirmation:"",
+    message:""
+  }
+  formSubmit=(e)=>{
+    e.preventDefault();
+    const data={
+      name:this.state.name,
+      email:this.state.email,
+      password:this.state.password,
+      password_confirmation:this.state.password_confirmation,
+    }
+    axios.post('register',data)
+    .then((response)=>{
+      localStorage.setItem('token',response.data.token);
+      this.setState({
+        loggedIn:true
+      })
+      this.props.setUser(response.data.user);
+    })
+  }
   render() {
     return (
       <div>
@@ -9,24 +33,24 @@ export default class Register extends Component {
           <div className="jumbotron row"><br></br>
               <div className="jumbotron col-md-4 offset-lg-4"><br></br>
                 <div className='card' style={{ backgroundColor: '#C2C0BE' }}>
-                    <form>
+                    <form onSubmit={this.formSubmit}>
                         <h2 className="text-center">Register Page</h2>
                         <div className="mb-3">
                             <label htmlFor="name" className="form-label">User Name</label>
-                            <input type="text" name="name" className="form-control" id="name" aria-describedby="name"/>
+                            <input type="text" name="name" className="form-control" id="name" required onChange={(e)=>{this.setState({name:e.target.value})}}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email address</label>
-                            <input type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+                            <input type="email" name="email" className="form-control" id="email" required onChange={(e)=>{this.setState({email:e.target.value})}}/>
                             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" name="password" className="form-control" id="password"/>
+                            <input type="password" name="password" className="form-control" id="password" required onChange={(e)=>{this.setState({password:e.target.value})}}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Confirm Password</label>
-                            <input type="password" name="confirmpassword" className="form-control" id="confirmpassword"/>
+                            <input type="password" name="password_confirmation" className="form-control" id="password_confirmation" required onChange={(e)=>{this.setState({password_confirmation:e.target.value})}}/>
                         </div>
                         <div className="mb-3 form-check">
                             <input type="checkbox" className="form-check-input" name='checkbox' id="checkbox"/>
--- laravel_react/app/Http/Controllers/AuthController.php
+++ laravel_react/app/Http/Controllers/AuthController.php
@@ -4,26 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Str;
+use App\Models\User;
 use DB;
 use Laravel\Passport\HasApiTokens;
-use App\Models\User;
+
 use Illuminate\Support\Facades\Hash;
 use Validator;
 class AuthController extends Controller
 {
     public function login(Request $request){
-        if(Auth::attempt($request->only('email','password'))){
-            $user=Auth::user();
-            $token = $user->createToken('app')->accessToken;
-            return response([
-                'message'=>"Successfully",
-                'token'=>$token,
-                'user'=>$user
-            ],200);
+        // if(Auth::attempt($request->only('email','password'))){
+        //     $user=Auth::user();
+        //     $token = $user->createToken('email')->accessToken;
+        //     return response([
+        //         'message'=>"Successfully",
+        //         'token'=>$token,
+        //         'user'=>$user
+        //     ],200);
+        // }
+        // return response([
+        //     'message' =>'InvalidEmail or Password'
+        // ],401);
+
+        if($request->isMethod('post')){
+            $data = $request->input();
+            //echo"<pre>";print_r($data);die;
+            $rules = [
+                "email"=> "required|email|exists:users",
+                "password"=> "required"
+             ];
+            $customMessages = [
+            'email.required' => 'Email is required',
+            'email.email'=>'valid email is required',
+            'email.unique'=>'Email does not exist in database',
+            'password.required' => 'pass word required',
+            ];
+    
+            $validator = Validator::make($data,$rules,$customMessages);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(),422);
+            }
+            $userDetails = User::where('email',$data['email'])->first();
+            if(password_verify($data['password'], $userDetails->password)){
+                $apiToken = Str::random(60);
+                User::where('email',$data['email'])->update(['access_token'=>$apiToken]);
+                return response()->json(['status'=>true,'message'=>'login successfully!','token'=>$apiToken],201);
+            }else{
+                return response()->json(["status"=>false,'message'=>'Password or not match!'],422);
+            }
         }
-        return response([
-            'message' =>'InvalidEmail or Password'
-        ],401);
     }
     public function register(Request $request) {
         if($request->isMethod('post')){
